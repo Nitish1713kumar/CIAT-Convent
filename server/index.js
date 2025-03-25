938598150c2073bb6e5f9662e1749fca9e99a61a
kavygupt <kavygupt@cisco.com>
2025-03-25 18:20:16 +0530

    updated package.json

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use('/api/uploadImageLinks', imageLinkRoutes);
 // Connect to MongoDB
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) => res.send('Hello!'));
